c521a1cf6406f7151e2ce3d8e70797c5d2c1b238
LucasCastro27 <14612571665@estudante.ufjf.br>
2024-07-18 06:19:30 -0300

    teste

--- jogo.hs
+++ jogo.hs
@@ -6,5 +6,5 @@ import System.Random
 
 rolarDados :: Int -> IO [Int]
 rolarDados quantidade = do
-    gen <- newStdGen  
+    gen <- newStdGen                                    --res
     return $ take quantidade $ randomRs (1, 6) gen      --faz a rolagem do dado, ou seja sorteia um numero aleatorio entre 1,6
